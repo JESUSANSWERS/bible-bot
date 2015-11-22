2bb990bec01655063d18b603c98d4f4fa19fc222
Thom Parkin <Thom.Parkin@Websembly.com>
2015-11-22 01:15:42 +0000

    Weird errors.  Tracking it down

--- bible.rb
+++ bible.rb
@@ -49,19 +49,13 @@ post '/bible' do
     book = result["book"].first
     puts "BOOK: #{book}"
     if book.nil? || book.empty? then exception("#{book} is null") end
-=begin
-  puts book
-  puts book["book_name"]  #confirm the book name
-  puts book["book_ref"] #match the book reference (abbreviation)
-  puts book["book_nr"]  #book number
-  puts book["chapter_nr"] #chapter number
-=end
     
     book_name = book["book_name"]
     chapter_number = book["chapter_nr"]
     verse_number = book["chapter"].first[0]
     verse_text = book["chapter"][verse_number.to_s]["verse"]
     verse_text.gsub!(/(god)/i, CAPS_PATTERN).gsub!(/(jesus)/i, CAPS_PATTERN) if HIGHLIGHT_GOD_REFERENCES
+      puts "book_name: #{book_name}\nchapter_number: #{chapter_number}\nverse_number: #{verse_number}\nverse_text: #{verse_text}\n\n"
     response_message = ":church: \nBible Verse  #{book_name} #{chapter_number}:#{verse_number}\n#{verse_text}"
   rescue 
     puts "ERROR: #{$!.message}"
