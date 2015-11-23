c68bfe763bdfb813c4db67449bf22847a94a08b2
Thom Parkin <Thom.Parkin@Websembly.com>
2015-11-23 21:22:28 +0000

    Rspec testing framework in place. Now I can add features easily

--- spec/bible_spec.rb
+++ spec/bible_spec.rb
@@ -1,18 +1,26 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe "Bible Bot for Slack" do
+  let(:url) { '/bible' }
+  
   it "should not respond to a default route" do
     get '/'
     expect(last_response).not_to be_ok
   end
   
   it "should respond to /bible" do
-    post '/bible'
+    post url, {
+      :user_name => "thom",
+      :trigger_word => "gospel",
+      :text => "gospel John 3:16"
+    }
     expect(last_response).to be_ok
   end
   
-  it "should respond with JSON" do
-    pending("Verify JSON is returned")
-    fail
+  it "should provide a single verse" do
+    post url, { :text => "bible John 3:16", :trigger_word => "bible"}
+    response = JSON.parse(last_response.body)
+    expect(response["text"]).to include("so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.")
   end
+  
 end
