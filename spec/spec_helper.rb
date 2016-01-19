2c94ca2eee13539b18fbb49f39255bd3d5205044
Thom Parkin <Thom.Parkin@Websembly.com>
2016-01-19 00:23:39 +0000

    Working on specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'rspec'
 require File.expand_path '../../bible.rb', __FILE__
 
 ENV['RACK_ENV'] = 'test'
+ENV['REDIS_URL'] = "redis://h:p7d7tq2p2auh2958o5qcid1seda@ec2-54-83-33-255.compute-1.amazonaws.com:17079"
 
 module RSpecMixin
   include Rack::Test::Methods
